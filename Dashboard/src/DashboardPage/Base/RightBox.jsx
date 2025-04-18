2fff2e457d96b50512779b296d4ad55a0860e5c4
Divyansh Sharma <shan17div@gmail.com>
2025-04-18 17:14:03 +0530

    LeftBox completed

--- Dashboard/src/DashboardPage/Base/RightBox.jsx
+++ Dashboard/src/DashboardPage/Base/RightBox.jsx
@@ -5,29 +5,38 @@ export default function RightBox() {
     return (
         <div className={styles.RightBox}>
             <div className={styles.heading}>
-                <h3 style={{ overflowY: "hidden" }} className="mb-4">Hi, User!</h3>
+                <h3 style={{ overflowY: "hidden" }} className="mb-4">
+                    Hi, User!
+                </h3>
             </div>
-            <div className="Equity">
-                <h4><i class="fa-solid fa-clock"></i> Equity</h4>
-                <div className="row">
-                    <div className="col-6">
+            <div className={`mb-5 ${styles.Equity}`}>
+                <h4>
+                    <i style={{ fontSize: "14px" }} class="fa-solid fa-clock"></i> Equity
+                </h4>
+                <div className={`row ${styles.EquityBox}`}>
+                    <div className={`p-3 col-2 ${styles.EquityBoxLeft}`}>
                         <h1>3.74k</h1>
                         <p>Margin available</p>
                     </div>
-                    <div className="col-6">
+                    <div className=" p-3 px-5 col-10">
                         <p>Margins used &nbsp; 0</p>
                         <p>Opening Balance &nbsp; 3.74k</p>
                     </div>
                 </div>
             </div>
-            <div className="Holdings">
-                <h4><i class="fa-solid fa-briefcase"></i> Holdings</h4>
-                <div className="row">
-                    <div className="col-6">
-                        <h1>1.55k + 5.20%</h1>
+            <div className={styles.Holdings}>
+                <h4>
+                    <i style={{ fontSize: "14px" }} class="fa-solid fa-briefcase"></i>{" "}
+                    Holdings (13)
+                </h4>
+                <div className={`row ${styles.HoldingBox}`}>
+                    <div className={`p-3 col-3 ${styles.HoldingBoxLeft}`}>
+                        <h1 style={{ color: "green" }}>
+                            1.55k <sub>+5.20%</sub>
+                        </h1>
                         <p>P&L</p>
                     </div>
-                    <div className="col-6">
+                    <div className=" p-3 px-5 col-9">
                         <p>Current Value &nbsp; 31.43k</p>
                         <p>Investment &nbsp; 29.88k</p>
                     </div>
