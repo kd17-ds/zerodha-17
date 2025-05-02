1ec1783242e6f21083bc162aca9af412db2d81d5
Divyansh Sharma <shan17div@gmail.com>
2025-05-02 14:15:10 +0530

    Holding completed

--- Dashboard/src/DashboardPage/Holdings/RightBox.jsx
+++ Dashboard/src/DashboardPage/Holdings/RightBox.jsx
@@ -7,11 +7,23 @@ export default function RightBox() {
         <div className={styles.RightBox}>
             <Navbar />
             <div className={styles.heading}>
-                <p >
-                    Holdings (13)
-                </p>
+                <p>Holdings (13)</p>
             </div>
             <HoldingTable />
+            <div className="row mx-3">
+                <div className={`p-3 col-4 ${styles.HoldingBoxLeft}`}>
+                    <h1 style={{ overflow: "hidden" }}>
+                        29,875.<sub>55</sub>
+                    </h1>
+                    <p>Total Investment</p>
+                </div>
+                <div className={`p-3 col-5 ${styles.HoldingBoxLeft}`}>
+                    <h1 style={{ overflow: "hidden" }}>
+                        31,428.<sub>95</sub>
+                    </h1>
+                    <p>Current Value</p>
+                </div>
+            </div>
         </div>
     );
 }
