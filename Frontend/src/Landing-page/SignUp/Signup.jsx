49a86ed264ba473b3f2da08f74563c1932a72a80
Divyansh Sharma <shan17div@gmail.com>
2025-06-14 23:12:21 +0530

    Updates

--- Frontend/src/Landing-page/SignUp/Signup.jsx
+++ Frontend/src/Landing-page/SignUp/Signup.jsx
@@ -1,7 +1,7 @@
 export default function Signup() {
     return (
         <div>
-            Signup Using <a href="https://zerodha-17-dashboard.onrender.com/"></a>
+            Signup Using <a href="https://zerodha-17-dashboard.onrender.com/">G0</a>
         </div>
     )
 }
