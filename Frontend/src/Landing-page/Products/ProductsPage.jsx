c13c501b857a7b20dd0b79c477f2568b98d70b34
Divyansh Sharma <shan17div@gmail.com>
2025-06-14 21:53:02 +0530

    updated frontedn images

--- Frontend/src/Landing-page/Products/ProductsPage.jsx
+++ Frontend/src/Landing-page/Products/ProductsPage.jsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         <>
             <Hero />
             <LeftImg
-                imageUrl={"media//images/kite.png"}
+                imageUrl={"media/images/kite.png"}
                 productName={"Kite"}
                 productDescr={
                     "Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
@@ -20,7 +20,7 @@ export default function HomePage() {
             />
 
             <RightImg
-                imageUrl={"media//images/console.png"}
+                imageUrl={"media/images/console.png"}
                 productName={"Console"}
                 productDescr={
                     "The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
@@ -29,7 +29,7 @@ export default function HomePage() {
             />
 
             <LeftImg
-                imageUrl={"media//images/coin.png"}
+                imageUrl={"media/images/coin.png"}
                 productName={"Coin"}
                 productDescr={
                     "Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
@@ -40,7 +40,7 @@ export default function HomePage() {
             />
 
             <RightImg
-                imageUrl={"media//images/kiteconnect.png"}
+                imageUrl={"media/images/kiteconnect.png"}
                 productName={"Kite Connect API"}
                 productDescr={
                     "Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
@@ -49,7 +49,7 @@ export default function HomePage() {
             />
 
             <LeftImg
-                imageUrl={"media//images/varsity.png"}
+                imageUrl={"media/images/varsity.png"}
                 productName={"Varsity mobile"}
                 productDescr={
                     "An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
